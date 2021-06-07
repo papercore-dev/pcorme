74e7dcf501e1a94c951d983848c0d8c3a01bfd1d
PaperCore <gyubini38@gmail.com>
2021-06-07 14:53:20 +0900

    Add files via upload

---
+++ dependencies.php
@@ -0,0 +1,23 @@
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<link rel="apple-touch-icon" sizes="180.180" href="https://pcor.me/identity/apple-touch-icon.png">
+<link rel="icon" type="image/png" sizes="192x192"  href="https://pcor.me/identity/android-chrome-192x192.png">
+<link rel="icon" type="image/png" sizes="32x32" href="https://pcor.me/identity/favicon-32x32.png">
+<link rel="icon" type="image/png" sizes="16x16" href="https://pcor.me/identity/favicon-16x16.png">
+<link rel="manifest" href="https://pcor.me/identity/site.webmanifest">
+<meta name="msapplication-TileColor" content="#1D4ED8">
+<meta name="theme-color" content="#1D4ED8">
+<title>PCorMe </title>
+<meta name="title" content="PCorMe ">
+<meta name="description" content="매우 짧은 URL 주소를 생성합니다!">
+<meta property="og:type" content="website">
+<meta property="og:url" content="https://pcor.me/">
+<meta property="og:title" content="PCorMe ">
+<meta property="og:description" content="매우 짧은 URL 주소를 생성합니다!">
+<meta property="og:image" content="https://pcor.me/identity/PCorMe_new.png">
+<meta property="twitter:card" content="summary_large_image">
+<meta property="twitter:url" content="https://pcor.me/">
+<meta property="twitter:title" content="PCorMe ">
+<meta property="twitter:description" content="매우 짧은 URL 주소를 생성합니다!">
+<meta property="twitter:image" content="https://pcor.me/identity/PCorMe_new.png">
+<!--<link href="https://pcor.me/code/unlinkui.css" rel="stylesheet">-->
+<link href="https://cdnjs.cloudflare.com/ajax/libs/tailwindcss/2.0.3/tailwind.min.css" rel="stylesheet">
