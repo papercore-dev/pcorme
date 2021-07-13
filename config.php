da081eef27c0ff531f090440835cd50d25d65ad9
PaperCore <gyubini38@gmail.com>
2021-07-13 18:16:37 +0900

    ㅠㅠ

--- config.php
+++ config.php
@@ -5,7 +5,7 @@ $hostname = 'https://pcor.me';
 // e.g - $connection = new PDO('mysql:dbname=Your Database name here;host=For synology - localhost:3007/ Others - localhost', 'User Name', 'Password');
 $connection = new PDO('mysql:dbname=;host=', '', '');
 // Choose your character set (default)
-$chars = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789가나다라마바사아자차카타파하';
+$chars = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789';
 $salt = '';
 $padding = 3;
 ?>
